ba0ce7aa9e301cc9368c22f0bab14df72b28d32e
Donghai Yu <815980445@qq.com>
2016-12-07 11:28:15 +0800

    add longitude and latitude index parameters
    this version support any size point in the trajectory, added longitude_index and latitude_index point out which parameters in a point is considered when calculating the distance.,,,,but now it just support 2-dimension distance,,,,I need to do something now.....

--- pyDP.py
+++ pyDP.py
@@ -10,81 +10,43 @@
 This is an implementation of Douglas-Peucker Algorithm, which can be used for trajectory compression.
 When there are enough points in a trajectory, this algorithm can find the key point in the trajectory.
 This tool support high dimension points.
-depend on: numpy, math
+depend on: numpy
 """
-import math
 import numpy as np
 
 
-EUCLIDIAN_DISTANCE = 1
-MANHATTAN_DISTANCE = 2
-
-
-def distance(a, b, method):
-    """Distance calculation between two points.
-    Args:
-        a: one of the two points.
-        b: another point of the two points.
-        method: distance calculation method.
-    Returns:
-        The distance between a and b based on method.
-    :param a: point a
-    :param b: point b
-    :param method: distance calculation method
-    :return: distance between a and b.
-    """
-    dist = float('inf')
-    if len(a) != len(b):
-        print("different size between A and B")
-        return dist
-    a_f = []
-    b_f = []
-    for i in range(0, len(a)):
-        try:
-            a_f.append(float(a[i]))
-            b_f.append(float(b[i]))
-        except TypeError as error:
-            print("can't trans the points to float list")
-            break
-    if method == EUCLIDIAN_DISTANCE:
-        ab_sum = 0
-        for i in range(len(a_f)):
-            ab_sum += pow((a_f[i] - b_f[i]), 2)
-        dist = math.sqrt(ab_sum)
-    elif method == MANHATTAN_DISTANCE:
-        ab_sum = 0
-        for i in range(len(a_f)):
-            ab_sum += abs(a_f[i] - b_f[i])
-        dist = ab_sum
-    return dist
-
-
-def distance_point2line(a, b, c):
+def distance_point2line(a, b, c, longitude_index, latitude_index):
     """Calculate the distance from a point to a line, when the line is decided by two points.
     the equation: |vector(ac)×vector(bc)|/vector(ab), a, b decide a line, c is the target point.
     :param a: the first line point
     :param b: the second line point
     :param c: the target point
+    :param longitude_index: the longitude index in a point
+    :param latitude_index: the latitude index in a point
     :return: the distance to line which is decided by point a and point b
     """
     if len(a) != len(b) or len(a) != len(c):
         print("there are different size between the points!")
         return
-    a_f = np.array(a, dtype="float32")
-    b_f = np.array(b, dtype="float32")
-    c_f = np.array(c, dtype="float32")
+    if longitude_index >= len(a) or latitude_index >= len(a):
+        print("longitude index or latitude index out of range")
+        return
+    a_f = np.array([a[longitude_index], a[latitude_index]], dtype="float32")
+    b_f = np.array([b[longitude_index], b[latitude_index]], dtype="float32")
+    c_f = np.array([c[longitude_index], c[latitude_index]], dtype="float32")
 
     ab = a_f - b_f
     bc = b_f - c_f
     ac = a_f - c_f
-    print(ac * ab)
     return sum((ac - sum(ac * ab) / sum(ab**2) * ab)**2)**0.5
 
 
-def trajectory_compression(raw_trajectory, threshold):
+def trajectory_compression(raw_trajectory, threshold, longitude_index, latitude_index):
     """
     :param raw_trajectory: the trajectory data, exp: [[x1, y1], [x2, y2], ... , [xn, yn]]
     :param threshold: distance threshold
+    :param longitude_index: the longitude index in a point
+    :param latitude_index: the latitude index in a point
     :return: compressed trajectory
     """
     if len(raw_trajectory) < 2:
@@ -98,7 +60,7 @@ def trajectory_compression(raw_trajectory, threshold):
     key_arr = [0 for i in range(len(raw_trajectory))]
     key_arr[0] = 1
     key_arr[len(raw_trajectory)-1] = 1
-    find_far_point(raw_trajectory, 0, len(raw_trajectory)-1, threshold, key_arr)
+    find_far_point(raw_trajectory, 0, len(raw_trajectory)-1, longitude_index, latitude_index, threshold, key_arr)
     key_points = []
     for i in range(len(key_arr)):
         if key_arr[i] == 1:
@@ -106,7 +68,7 @@ def trajectory_compression(raw_trajectory, threshold):
     return key_points
 
 
-def find_far_point(arr, i, j, threshold, flag_arr):
+def find_far_point(arr, i, j, longitude_index, latitude_index, threshold, flag_arr):
     """
     This function is used for finding the farthest points (to line (arr[i], arr[j])) in sub trajectory arr(i, j),
     it is recursive until there are no more points's distance is larger than threshold to the line. if a point is not
@@ -114,6 +76,8 @@ def find_far_point(arr, i, j, threshold, flag_arr):
     :param arr: trajectory data
     :param i: start index
     :param j: end index
+    :param longitude_index: the longitude index in a point
+    :param latitude_index: the latitude index in a point
     :param threshold: distance threshold
     :param flag_arr: the flag array of useful or ignore for the trajectory points(0:ignore, 1:useful)
     :return: none, but change the flag_arr
@@ -124,18 +88,19 @@ def find_far_point(arr, i, j, threshold, flag_arr):
     max_distance = float(0)
     flag = -1
     for k in range(i + 1, j):
-        d = distance_point2line(arr[i], arr[j], arr[k])
-        print(d)
+        d = distance_point2line(arr[i], arr[j], arr[k], longitude_index, latitude_index)
         if d > max_distance:
             max_distance = d
             flag = k
     if max_distance > threshold:
         flag_arr[flag] = 1
-        find_far_point(arr, i, flag, threshold, flag_arr)
-        find_far_point(arr, flag, j, threshold, flag_arr)
+        find_far_point(arr, i, flag, longitude_index, latitude_index, threshold, flag_arr)
+        find_far_point(arr, flag, j, longitude_index, latitude_index, threshold, flag_arr)
 
-arrs = []
-# narrs = trajectory_compression(arrs, "0.0007", EUCLIDIAN_DISTANCE)
-# print(narrs)
-# print(distance_point2line([1, 0], [2, 0], [0.5, 1]))
+'''
+arrs = [[1, 0, 'x'], [2, 1, 'y'], [3, 2, 'z']]
+narrs = trajectory_compression(arrs, "0.0007", 0, 1)
+print(narrs)
+'''
+# print(distance_point2line([1, 0], [2.6, 1], [3.5, 6]))
 
