aece0524ba6b6e9724cdb267133a589326d412d8
Donghai Yu <815980445@qq.com>
2016-12-07 13:55:05 +0800

    Update pyDP.py

--- pyDP.py
+++ pyDP.py
@@ -10,7 +10,7 @@
 This is an implementation of Douglas-Peucker Algorithm, which can be used for trajectory compression.
 When there are enough points in a trajectory, this algorithm can find the key point in the trajectory.
 This tool support high dimension points.
-depend on: numpy, python3.5
+depend on: numpy 1.11.2, python 3.5.2
 """
 import numpy as np
 
